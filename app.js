28c89c094113a0f39a27adab2518738392e4c814
Jisan Jung <jjjung13@gmail.com>
2019-07-28 17:34:56 -0400

    mobile menu fixed, working on showcase

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var mobileMenu = document.getElementById("mobile-menu");
 mobileMenu.addEventListener("click", function() {
     var mobileNav = document.getElementById("mobile-nav");
 
-    if (mobileNav.style.opacity === "1") {
+    if (mobileNav.style.transform === "translateY(0px)" && mobileNav.style.opacity === "1") {
+        mobileNav.style.transform = "translateY(-150px)";
         mobileNav.style.opacity = "0";
         } else {
+            mobileNav.style.transform = "translateY(0px)";
             mobileNav.style.opacity = "1";
         }
 });
