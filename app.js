fd204b8dda7c95932d4dd5667b6653ad0de1db38
Jisan Jung <jjjung13@gmail.com>
2019-07-30 01:38:09 -0400

    finished showcase

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var mobileMenu = document.getElementById("mobile-menu");
 
-mobileMenu.addEventListener("click", function() {
+mobileMenu.addEventListener("click", menuToggle);
+
+function menuToggle() {
     var mobileNav = document.getElementById("mobile-nav");
 
     if (mobileNav.style.transform === "translateY(0px)" && mobileNav.style.opacity === "1") {
-        mobileNav.style.transform = "translateY(-150px)";
+        mobileNav.style.transform = "translateY(-250px)";
         mobileNav.style.opacity = "0";
         } else {
             mobileNav.style.transform = "translateY(0px)";
             mobileNav.style.opacity = "1";
         }
-});
+}
