919599e9d9d23d60d4d36bfa82bac28ad8a31ccc
Jisan Jung <jjjung13@gmail.com>
2019-08-08 15:44:24 -0400

    added little bit of responsiveness

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ mobileMenu.addEventListener("click", menuToggle);
 
 function menuToggle() {
     var mobileNav = document.getElementById("mobile-nav");
+    var deviceWidth = window.matchMedia("(min-width: 475px)");
 
     if (mobileNav.style.transform === "translateY(0px)" && mobileNav.style.opacity === "1") {
         mobileNav.style.transform = "translateY(-250px)";
